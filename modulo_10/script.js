e470a6946c7ea107d116417d4525c51c11f5104c
brunops52 <139251367+brunops52@users.noreply.github.com>
2023-10-27 10:10:25 -0300

    Update script.js
    consertando a máscara no campo CPF e CEP

--- modulo_10/script.js
+++ modulo_10/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         e.preventDefault();
     })
     $('#telefone').mask('(00) 00000-0000');
-    $('#cpf').mask('00000000000');
-    $('#cep').mask('00000000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
     $('form').validate({
         rules: {
             cpf:{
