38660c6ddb27f0489f5a53389e63f3884fe03234
bruno ps <bunros52@gmail.com>
2023-11-28 01:10:06 -0300

    adicionando tarefa

---
+++ tarefa_tsc/tarefa_funcoes.js
@@ -0,0 +1,9 @@
+"use strict";
+function multplica(primeiro, segundo) {
+    return primeiro * segundo;
+}
+function saudacao(nome) {
+    console.log(`Olá ${nome}`);
+}
+console.log(multplica(2, 3));
+saudacao("bruno");
---
+++ tarefa_tsc/tarefa_funcoes.ts
@@ -0,0 +1,12 @@
+function multplica(primeiro: number,segundo: number) {
+    return primeiro * segundo;
+}
+
+
+function saudacao(nome: string) {
+    console.log(`Olá ${nome}`);
+}
+
+console.log(multplica(2,3))
+
+saudacao("bruno")
